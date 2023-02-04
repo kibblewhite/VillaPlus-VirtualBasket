5c5cca85ee5ae9b0fbc542c2d2884225db197c8d
Kibble White <kibblewhite@live.com>
2023-02-04 03:29:25 +0000

    Refactor code and a display of code progression

--- VirtualBasket/DiscountRules/BuyTwoGetOneFreeDiscountRule.cs
+++ VirtualBasket/DiscountRules/BuyTwoGetOneFreeDiscountRule.cs
@@ -9,22 +9,14 @@ public class BuyTwoGetOneFreeDiscountRule : IDiscountRule
     public ValueResults<int> GetDiscount(Basket basket)
     {
         int itemCount = 0;
+        List<BasketItem> discountedValueItems = new();
 
         try
         {
             itemCount = basket.Items.Count(i => ItemsGuidList.Contains(i.Id) is true);
-        }
-        catch (Exception ex)
-        {
-            return ValueResults<int>.Failed(ex.HResult, ex.Message);
-        }
+            int freeItems = itemCount / 3;
 
-        int freeItems = itemCount / 3;
-
-        // In these two lines, items ordered by their cheapest value is subtracted, instead of matching by batches of matched item
-        List<BasketItem> discountedValueItems = new();
-        try
-        {
+            // In these two lines, items ordered by their cheapest value is subtracted, instead of matching by batches of matched item
             IOrderedEnumerable<BasketItem> itemsOrderedByCheapest = basket.Items.OrderBy(x => x.Price);
             IEnumerable<BasketItem> discountedItems = itemsOrderedByCheapest.Take(freeItems);
             discountedValueItems.AddRange(discountedItems);
